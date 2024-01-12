cd3beeff0ab99870bbbab09688ed0ca917f2b9ee
Luis Sanchez <luis.sanchez.dev@gmail.com>
2024-01-12 16:04:44 -0500

    disabled optimized images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    // no optimizar las imagenes
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
